37ec52b4f739df3b742809c5a17ac4f55032329f
divineLush <alvipath@yahoo.com>
2021-11-27 06:48:13 -0500

    dockerize app

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const http = require('http')
-const path = require('path')
-
 const ex = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 
 const viewRoutes = require('./routes/view')
 const error = require('./controllers/error')
 
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const ItemSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
 const app = ex()
+
+const PORT = 3000;
 
 app.set('view engine', 'pug')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(ex.static(path.join(__dirname, 'public')))
-
 app.use(viewRoutes)
-
 app.use(error.getNotFound)
 
-const server = http.createServer(app)
-server.listen(3000)
+app.listen(PORT)
--- utils/path.js
+++ utils/path.js
@@ -2,4 +2,4 @@ const path = require("path")
 
 const root = path.dirname(require.main.filename)
 
-exports.inputPath = path.join(root, 'data', 'input', 'input.json')
+exports.inputPath = path.join(root, 'usrfiles', 'input', 'input.json')
